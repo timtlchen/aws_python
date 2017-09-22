5e5644fc678e7b3d58072efbf31d5bb7ba92050b
timchen <cjh0313@163.com>
2017-09-23 02:11:52 +0800

    correct s3 upload function

--- aws_s3_upload.py
+++ aws_s3_upload.py
@@ -5,8 +5,7 @@ class aws_s3_upload:
     def __init__(self,aws_id,aws_key,aws_bucket):
         conn = tinys3.Connection(aws_id,aws_key,tls=True,endpoint='ap-southeast-1.amazonaws.com')
         f = open('/opt/aws_python/test.txt','rb')
-        print conn.upload('test1.txt',f,aws_bucket)
-        print conn.get('test1.txt',aws_bucket)
+        conn.upload('test1.txt',f,aws_bucket)
 
 c = aws_s3_upload(aws_id,aws_key,aws_bucket)
 result = "[" + c.result + "]"
