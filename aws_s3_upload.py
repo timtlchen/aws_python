eec8491f3cb9ef0fae9afddc41c4201b196fad32
timchen <cjh0313@163.com>
2017-09-23 01:27:36 +0800

    debug

--- aws_s3_upload.py
+++ aws_s3_upload.py
@@ -6,7 +6,11 @@ AWS_ACCESS_KEY_ID = sys.argv[1]
 AWS_SECRET_ACCESS_KEY = sys.argv[2]
 bucket_name = sys.argv[3]
 
+print("debug0")
+
 conn = tinys3.Connection(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY,tls=True,endpoint='ap-southeast-1.amazonaws.com')
+
+print("debug1")
 
 f = open('test.txt','rb')
 print conn.upload('test1.txt',f,bucket_name)
